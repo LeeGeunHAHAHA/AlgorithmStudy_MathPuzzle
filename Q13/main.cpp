07ce7e59664bc6ff4851eccb00151d23d2fee4f7
LeeGeunHAHAHA <ygh1kr@naver.com>
2017-09-28 21:16:29 +0900

    why?

--- Q13/main.cpp
+++ Q13/main.cpp
@@ -1,6 +1,8 @@
+
 #include<iostream>
 #include<algorithm>
 #include<vector>
+#include<cmath>
 using namespace std;
 bool func(int i, int j){
     int a = i; int b = j;
@@ -12,28 +14,35 @@ int main(){
     int testCase;
     cin>>testCase;
     while(testCase--){
-    int n, k;
-    cin>>n>>k;
-    int * arr = new int[n];
-    vector<int> sums;
-    for(int i =0; i<n ; i++){
-        int tmp;
-        cin>>tmp;
-        arr[i] =tmp;
-    }
-    for(int i =0; i<n; i++){
-        for(int j =i+1; j<n; j++){
-            sums.push_back(arr[i]+arr[j]-k);            
+        int n, k;
+        cin>>n>>k;
+        int* arr=new int[n];
+        for(int i =0; i<n ; i++){
+            int tmp;
+            cin>>tmp;
+            arr[i]=tmp;
         }
+        sort(arr,arr+n);
+        int index=0;
+        vector<int> sums;
+        for(int i =0; i< n; i++){
+            int lower = lower_bound(arr+i, arr+n, k-arr[i])-arr;
+            if(abs(arr[i]+arr[lower-1]-k)<abs(arr[i]+arr[lower]-k))index =lower-1;
+            else index =lower;
+            if(i!= index){
+                if(sums.size()==0)
+                    sums.push_back(arr[i]+arr[index]-k);
+                else{
+                    if(abs(sums[0]-k)>abs(arr[i]+arr[index]-k))sums[0]=arr[i]+arr[index]-k;
+                    else if(abs(sums[0]-k)==abs(arr[i]+arr[index]-k))sums.push_back(arr[i]+arr[index]-k);
+                }
+            }
+
+        }
+
+
+        cout<<sums.size()<<endl;
     }
-    sort(sums.begin(),sums.end(),func);
-    int cnt =1;
-    for(int i=0; i<sums.size(); i++){
-    if(sums[i]==sums[i+1] || sums [i]==-sums[i+1]) cnt+=1;
-    else break;
-    }
-    cout<<cnt<<endl;
-    delete arr;
-    }
-     return 0;
+    return 0;
 }
+
