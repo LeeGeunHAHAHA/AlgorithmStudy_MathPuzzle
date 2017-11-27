fe7d50721cd04ff4987e00e8b05c10f8d860c4a0
LeeGeunHaHaHa <ygh1kr@naver.com>
2017-11-27 23:56:25 +0900

    tmp

--- Q75_boj_2294/Main.py
+++ Q75_boj_2294/Main.py
@@ -2,6 +2,8 @@ n, k = map(int, input().split())
 coins = [int(input()) for i in range(n)]
 d=[[0 for i in range(100001)] for i in range (n)]
 for i in range (n) : d[i][0] =1
+for i in range(n):
+    print(d[i][0:k+1])
 
 for i in range(n) :
     for j in range (1,k+1):
@@ -9,6 +11,7 @@ for i in range(n) :
             d[i][j] = d[i-1][j-coins[i]] + d[i-1][j]
         else : 
             d[i][j] = d[i-1][j]
-    print (d[i][0:k+1])
-
+    print (d[i-1][0:k+1])
+for i in range(n):
+    print(d[i][0:k+1])
 print(d[n-1][k])
